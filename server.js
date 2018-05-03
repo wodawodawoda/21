b8a339e9ae2828580d55292eb916484842bf067b
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-05-03 22:26:49 +0200

    Add database to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs')
 
 
 
